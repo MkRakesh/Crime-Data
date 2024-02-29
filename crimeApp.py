0a82c48c25816305ead14c9d5c0485bc0c7d9cbc
Rakesh M K <mkk.rakesh@gmail.com>
2024-02-29 23:01:26 +0530

    Update crimeApp.py

--- crimeApp.py
+++ crimeApp.py
@@ -1,1 +1,182 @@
-c
+NameError: name 'c' is not defined
+
+[17:29:42] ❗️ 
+
+2024-02-29 17:29:42.105 503 GET /script-health-check (10.12.30.129) 10.42ms
+
+2024-02-29 17:29:47.225 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:29:47.431 503 GET /script-health-check (10.12.30.129) 208.27ms
+
+2024-02-29 17:29:47.913 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:29:52.110 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:29:52.110 503 GET /script-health-check (10.12.30.129) 5.08ms
+
+2024-02-29 17:29:57.101 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:29:57.155 503 GET /script-health-check (10.12.30.129) 55.29ms
+
+2024-02-29 17:30:02.108 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:02.109 503 GET /script-health-check (10.12.30.129) 1.76ms
+
+2024-02-29 17:30:07.070 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:07.200 503 GET /script-health-check (10.12.30.129) 132.69ms
+
+2024-02-29 17:30:12.048 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:12.239 503 GET /script-health-check (10.12.30.129) 193.05ms
+
+2024-02-29 17:30:17.079 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:17.268 503 GET /script-health-check (10.12.30.129) 191.08ms
+
+2024-02-29 17:30:22.053 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:22.053 503 GET /script-health-check (10.12.30.129) 1.77ms
+
+2024-02-29 17:30:27.070 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:27.246 503 GET /script-health-check (10.12.30.129) 177.54ms
+
+2024-02-29 17:30:32.176 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/crime-data/crimeApp.py", line 1, in <module>
+
+    c
+
+NameError: name 'c' is not defined
+
+2024-02-29 17:30:32.234 503 GET /script-health-check (10.12.30.129) 59.39ms
+
+main
+mkrakesh/crime-data/main/crimeApp.py
